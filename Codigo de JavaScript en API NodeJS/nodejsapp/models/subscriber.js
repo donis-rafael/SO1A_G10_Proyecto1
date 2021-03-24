a71bedbe4942a82e2ffaa70ca64a3e577589c2d4
goodideasgt <mafuentes@goodideasgt.com>
2021-03-23 23:27:40 -0600

    se agrego codigo de node js

---
+++ Codigo de JavaScript en API NodeJS/nodejsapp/models/subscriber.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const subscriberSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true 
+  },
+  infectedtype: {
+    type: String,
+    required: true 
+  },
+  state: {
+    type: String,
+    required: true 
+  }
+})
+
+module.exports = mongoose.model('Subscriber', subscriberSchema)
