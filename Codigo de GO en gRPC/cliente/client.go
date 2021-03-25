30c38c3e566d48f38ae03c8b41747ad00d2f9141
goodideasgt <mafuentes@goodideasgt.com>
2021-03-25 03:24:31 -0600

    se agrega cambio a cliente de grpc para conexion con locust

--- Codigo de GO en gRPC/cliente/client.go
+++ Codigo de GO en gRPC/cliente/client.go
@@ -161,31 +161,15 @@ func http_server(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		// Unmarshal
-		var p Person
-		errs = json.Unmarshal(body, &p)
-		if errs != nil {
-			http.Error(w, errs.Error(), 500)
-			return
-		}
-
-		output, err := json.Marshal(p)
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
-		w.Header().Set("content-type", "application/json")
-		w.Write(output)
-		fmt.Println(">> BODY 1:  Iniciando  ", output)
 		r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 
 		fmt.Println(">> BODY: Iniciando  ", r.Body)
+		var p Person
 
 		dec := json.NewDecoder(r.Body)
 		dec.DisallowUnknownFields()
-		var p1 Person
-		errm := dec.Decode(&p1)
-		if errm != nil {
+		err := dec.Decode(&p)
+		if err != nil {
 			var syntaxError *json.SyntaxError
 			var unmarshalTypeError *json.UnmarshalTypeError
 
@@ -193,35 +177,35 @@ func http_server(w http.ResponseWriter, r *http.Request) {
 			// Catch any syntax errors in the JSON and send an error message
 			// which interpolates the location of the problem to make it
 			// easier for the client to fix.
-			case errors.As(errm, &syntaxError):
+			case errors.As(err, &syntaxError):
 				msg := fmt.Sprintf("Request body contains badly-formed JSON (at position %d)", syntaxError.Offset)
 				http.Error(w, msg, http.StatusBadRequest)
-				fmt.Println(">> CLIENT: Error 10 Recibiendo: ", errm.Error())
+				fmt.Println(">> CLIENT: Error 10 Recibiendo: ", err.Error())
 
 			// In some circumstances Decode() may also return an
 			// io.ErrUnexpectedEOF error for syntax errors in the JSON. There
 			// is an open issue regarding this at
 			// https://github.com/golang/go/issues/25956.
-			case errors.Is(errm, io.ErrUnexpectedEOF):
+			case errors.Is(err, io.ErrUnexpectedEOF):
 				msg := fmt.Sprintf("Request body contains badly-formed JSON")
 				http.Error(w, msg, http.StatusBadRequest)
-				fmt.Println(">> CLIENT: Error 11 Recibiendo: ", errm.Error())
+				fmt.Println(">> CLIENT: Error 11 Recibiendo: ", err.Error())
 
 			// Catch any type errors, like trying to assign a string in the
 			// JSON request body to a int field in our Person struct. We can
 			// interpolate the relevant field name and position into the error
 			// message to make it easier for the client to fix.
-			case errors.As(errm, &unmarshalTypeError):
+			case errors.As(err, &unmarshalTypeError):
 				msg := fmt.Sprintf("Request body contains an invalid value for the %q field (at position %d)", unmarshalTypeError.Field, unmarshalTypeError.Offset)
 				http.Error(w, msg, http.StatusBadRequest)
-				fmt.Println(">> CLIENT: Error 12 Recibiendo: ", errm.Error())
+				fmt.Println(">> CLIENT: Error 12 Recibiendo: ", err.Error())
 
 			// Catch the error caused by extra unexpected fields in the request
 			// body. We extract the field name from the error message and
 			// interpolate it in our custom error message. There is an open
 			// issue at https://github.com/golang/go/issues/29035 regarding
 			// turning this into a sentinel error.
-			case strings.HasPrefix(errm.Error(), "json: unknown field "):
+			case strings.HasPrefix(err.Error(), "json: unknown field "):
 				fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
 				msg := fmt.Sprintf("Request body contains unknown field %s", fieldName)
 				http.Error(w, msg, http.StatusBadRequest)
@@ -229,30 +213,30 @@ func http_server(w http.ResponseWriter, r *http.Request) {
 
 			// An io.EOF error is returned by Decode() if the request body is
 			// empty.
-			case errors.Is(errm, io.EOF):
+			case errors.Is(err, io.EOF):
 				msg := "Request body must not be empty"
 				http.Error(w, msg, http.StatusBadRequest)
-				fmt.Println(">> CLIENT: Error 14 Recibiendo: ", errm.Error())
+				fmt.Println(">> CLIENT: Error 14 Recibiendo: ", err.Error())
 
 			// Catch the error caused by the request body being too large. Again
 			// there is an open issue regarding turning this into a sentinel
 			// error at https://github.com/golang/go/issues/30715.
-			case errm.Error() == "http: request body too large":
+			case err.Error() == "http: request body too large":
 				msg := "Request body must not be larger than 1MB"
 				http.Error(w, msg, http.StatusRequestEntityTooLarge)
-				fmt.Println(">> CLIENT: Error 15 Recibiendo: ", errm.Error())
+				fmt.Println(">> CLIENT: Error 15 Recibiendo: ", err.Error())
 			// Otherwise default to logging the error and sending a 500 Internal
 			// Server Error response.
 			default:
-				log.Println(errm.Error())
+				log.Println(err.Error())
 				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-				fmt.Println(">> CLIENT: Error 16 Recibiendo: ", errm.Error())
+				fmt.Println(">> CLIENT: Error 16 Recibiendo: ", err.Error())
 			}
-			fmt.Println(">> CLIENT: Error Recibiendo: ", errm.Error())
+			fmt.Println(">> CLIENT: Error Recibiendo: ", err.Error())
 			return
 		}
 		// Obtener el nombre enviado desde la forma
-		name := p1.name //r.FormValue("name")
+		name := p.name //r.FormValue("name")
 		// Obtener el mensaje enviado desde la forma
 		location := r.FormValue("location")
 		age := r.FormValue("age") //strconv.Itoa(p.age)
