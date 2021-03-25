be50994120672b8aca39a86100120e8decec6a54
goodideasgt <mafuentes@goodideasgt.com>
2021-03-25 06:01:28 -0600

    se agrega cambio a cliente de grpc para conexion con locust

--- Codigo de GO en gRPC/cliente/client.go
+++ Codigo de GO en gRPC/cliente/client.go
@@ -115,6 +115,9 @@ func http_server(w http.ResponseWriter, r *http.Request) {
 
 		buf := new(bytes.Buffer)
 		buf.ReadFrom(myBody)
+		data := Person{}
+		json.Unmarshal([]byte(buf.String()), &data)
+		fmt.Printf("Nombre: %s", data.name)
 		fmt.Println(">> BODY: Iniciando 2 ", buf.String())
 
 		var p Person
