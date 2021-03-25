5471caea272b2dd9866c2e1a77b7e0565c011984
goodideasgt <mafuentes@goodideasgt.com>
2021-03-25 00:20:19 -0600

    se agrega cambio a cliente de grpc para conexion con locust

--- Codigo de GO en gRPC/cliente/client.go
+++ Codigo de GO en gRPC/cliente/client.go
@@ -119,6 +119,7 @@ func http_server(w http.ResponseWriter, r *http.Request) {
 		case errors.As(err, &syntaxError):
 			msg := fmt.Sprintf("Request body contains badly-formed JSON (at position %d)", syntaxError.Offset)
 			http.Error(w, msg, http.StatusBadRequest)
+			fmt.Println(">> CLIENT: Error 10 Recibiendo: ", err.Error())
 
 		// In some circumstances Decode() may also return an
 		// io.ErrUnexpectedEOF error for syntax errors in the JSON. There
@@ -127,6 +128,7 @@ func http_server(w http.ResponseWriter, r *http.Request) {
 		case errors.Is(err, io.ErrUnexpectedEOF):
 			msg := fmt.Sprintf("Request body contains badly-formed JSON")
 			http.Error(w, msg, http.StatusBadRequest)
+			fmt.Println(">> CLIENT: Error 11 Recibiendo: ", err.Error())
 
 		// Catch any type errors, like trying to assign a string in the
 		// JSON request body to a int field in our Person struct. We can
@@ -135,6 +137,7 @@ func http_server(w http.ResponseWriter, r *http.Request) {
 		case errors.As(err, &unmarshalTypeError):
 			msg := fmt.Sprintf("Request body contains an invalid value for the %q field (at position %d)", unmarshalTypeError.Field, unmarshalTypeError.Offset)
 			http.Error(w, msg, http.StatusBadRequest)
+			fmt.Println(">> CLIENT: Error 12 Recibiendo: ", err.Error())
 
 		// Catch the error caused by extra unexpected fields in the request
 		// body. We extract the field name from the error message and
@@ -145,12 +148,14 @@ func http_server(w http.ResponseWriter, r *http.Request) {
 			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
 			msg := fmt.Sprintf("Request body contains unknown field %s", fieldName)
 			http.Error(w, msg, http.StatusBadRequest)
+			fmt.Println(">> CLIENT: Error 13 Recibiendo: ", err.Error())
 
 		// An io.EOF error is returned by Decode() if the request body is
 		// empty.
 		case errors.Is(err, io.EOF):
 			msg := "Request body must not be empty"
 			http.Error(w, msg, http.StatusBadRequest)
+			fmt.Println(">> CLIENT: Error 14 Recibiendo: ", err.Error())
 
 		// Catch the error caused by the request body being too large. Again
 		// there is an open issue regarding turning this into a sentinel
@@ -158,12 +163,13 @@ func http_server(w http.ResponseWriter, r *http.Request) {
 		case err.Error() == "http: request body too large":
 			msg := "Request body must not be larger than 1MB"
 			http.Error(w, msg, http.StatusRequestEntityTooLarge)
-
+			fmt.Println(">> CLIENT: Error 15 Recibiendo: ", err.Error())
 		// Otherwise default to logging the error and sending a 500 Internal
 		// Server Error response.
 		default:
 			log.Println(err.Error())
 			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			fmt.Println(">> CLIENT: Error 16 Recibiendo: ", err.Error())
 		}
 		fmt.Println(">> CLIENT: Error Recibiendo: ", err.Error())
 		return
